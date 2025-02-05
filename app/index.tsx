d1740e51866c528c88a13736415982ac1c6e1bde
Batur <halidbaturakkurt@gmail.com>
2025-02-05 15:30:13 +0300

    feat: add date formatting and localization support
    - PAX-16

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,7 @@
 import { Text, View } from "react-native";
 
+import { format } from "date-fns";
+
 export default function Index() {
   return (
     <View
@@ -7,9 +9,11 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        gap: 16,
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>{format(new Date(), "dd/MM/yyyy")}</Text>
     </View>
   );
 }
