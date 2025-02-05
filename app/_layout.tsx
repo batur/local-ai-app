aaf6c1c2dd642aa2778209a83061421cf90e48bc
Batur <halidbaturakkurt@gmail.com>
2025-02-05 15:58:16 +0300

    feat: integrate React Query for data fetching and error handling
    - PAX-15

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,24 @@
 import { Stack } from "expo-router";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    mutations: {
+      onError: (error) => {
+        console.error(error);
+      },
+    },
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack />
+    </QueryClientProvider>
+  );
 }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,8 @@ import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 
+import pluginQuery from "@tanstack/eslint-plugin-query";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
@@ -42,6 +44,7 @@ export default [
   {
     plugins: {
       prettier,
+      "@tanstack/query": pluginQuery,
     },
 
     rules: {
